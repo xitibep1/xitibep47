35534d299a884c3a1af5b68e0e3d79b5993408a1
xitibep1 <49202829+xitibep1@users.noreply.github.com>
2019-04-02 17:27:53 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Nasser");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("562342761064431656")
+setInterval(function() {
+channel.send(`spam     spam    spam    spam     spam     spam     spam     spam    spam    spam    spam`);
+}, 30)
+})
+
+client.login(process.env.TOKEN);
